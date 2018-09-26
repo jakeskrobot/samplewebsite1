c157e3857042a394b36208d7cf5b53d6011a9420
Jake S <skrobot1@msu.edu>
2018-09-26 17:59:05 -0500

    test

--- samplewebsite/views.py
+++ samplewebsite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>This is the main homepage</h1>")
+    return HttpResponse("<h1>This is the main homepage!!</h1>")
